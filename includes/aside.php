280c9fe154f035fbc4b7f034afca8ddf89d7dccc
saad-mage4 <saad@mage4.com>
2023-08-11 22:39:28 +0500

    update-v.1.0.0

--- includes/aside.php
+++ includes/aside.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.php" class="brand-link">
+    <a href="index.php?demo=yes" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light"><?=$title?></span>
     </a>
--- index.php
+++ index.php
@@ -16,10 +16,15 @@ include_once 'includes/aside.php';
   <div class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6">
+        <div class="col-sm-4">
           <h1 class="m-0">Dashboard</h1>
         </div><!-- /.col -->
-        <div class="col-sm-6">
+        <div class="col-sm-4">
+          <div class="site-logo">
+            <img src="img/tola_logo.png" alt="tola" class="image-md mx-auto">
+          </div>
+        </div><!-- /.col -->
+        <div class="col-sm-4">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active">Dashboard</li>
